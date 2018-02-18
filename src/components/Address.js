61e0d53bb1b7990ce8bd1de8c7649a2aa0dd79ed
github username <github email address>
2018-02-18 09:40:10 -0500

    Done.

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,1 +1,15 @@
-// Component Code Goes Here
+import React from "react";
+
+class Address extends React.Component {
+  render() {
+    return (
+      <div className="address">
+        <h1>
+          {this.props.street}, {this.props.city}
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Address;
