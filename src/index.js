365dc8fae6151b40fcef32bd6264c89d8b533efe
github username <github email address>
2018-02-18 14:28:02 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import ToggleButton from './components/ToggleButton.js'
+import Address from './components/Address.js'
 ReactDOM.render(
-  <div>Components Need To Be Mounted</div>,
+  <div>
+    <ToggleButton />
+    <Address
+      street = "Carli Caplina"
+      city = "Beograd"
+    />
+  </div>,
   document.getElementById('root')
 )
