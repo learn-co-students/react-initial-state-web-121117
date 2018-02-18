a15b9bab8659d864aa1ce30ad241f1d66e9f9a41
julientregoat <julienctregoat@gmail.com>
2018-02-17 21:11:55 -0500

    Done.

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,1 +1,20 @@
-// Component Code Goes Here
+import React from 'react'
+
+class Address extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div className="address">
+       {this.props.street} {this.props.city}
+      </div>
+    )
+  }
+}
+
+export default Address;
